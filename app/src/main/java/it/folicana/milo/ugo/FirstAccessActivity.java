12ddcd1a97251f6621ac9f9eff94f76bff901b22
Nicola Falomi <nicola.falomi@studenti.polito.it>
2015-04-13 10:07:26 +0200

    No Leak
    aggiunta di una classe UiHandler statica e di weakreference all'activity
    per evitare memory leak

---
+++ app/src/main/java/it/folicana/milo/ugo/FirstAccessActivity.java
@@ -0,0 +1,13 @@
+package it.folicana.milo.ugo;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+
+public class FirstAccessActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
